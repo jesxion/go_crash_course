a18b1ffb2574f73dfa297a8b968922019f5afab5
jesxion <jesxion@gmail.com>
2021-03-18 13:52:59 +0800

    08_maps

--- 08_maps/main.go
+++ 08_maps/main.go
@@ -13,6 +13,8 @@ func main() {
 
 	// Decalre map and add kv
 	emails := map[string]string{"Bob": "bob@gmail.com", "Sharon": "sharon@gmail.com"}
+	emails["Mike"] = "mike@gmail.com"
+
 	fmt.Println(emails)
 	fmt.Println(len(emails))
 	fmt.Println(emails["Bob"])
